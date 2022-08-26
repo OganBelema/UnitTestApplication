38d8fd53111b06256385f7870ccae90dbb54289f
Oganbelema <oganbelema@gmail.com>
2022-08-26 16:35:25 +0100

    called start car engine in main activity

--- app/src/main/java/com/oganbelema/unittestapplication/MainActivity.kt
+++ app/src/main/java/com/oganbelema/unittestapplication/MainActivity.kt
@@ -2,9 +2,16 @@ package com.oganbelema.unittestapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.coroutines.withContext
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+
+        val engine = Engine()
+        val car = Car(20.0, engine)
+
+        car.turnOn()
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
