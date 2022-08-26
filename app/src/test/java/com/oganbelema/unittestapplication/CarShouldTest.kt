93d2a4a8b8c93c36961e863081d77d05c8db3cba
Oganbelema <oganbelema@gmail.com>
2022-08-26 18:18:52 +0100

    mocked turn on function for the engine in car test

--- app/src/test/java/com/oganbelema/unittestapplication/CarShouldTest.kt
+++ app/src/test/java/com/oganbelema/unittestapplication/CarShouldTest.kt
@@ -1,6 +1,8 @@
 package com.oganbelema.unittestapplication
 
 import com.example.outsideintddexample.acceptancetests.MainCoroutineScopeRule
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.test.runBlockingTest
 import org.junit.Assert.assertEquals
 import org.junit.Rule
@@ -8,12 +10,28 @@ import org.junit.Test
 import org.mockito.kotlin.mock
 import org.mockito.kotlin.times
 import org.mockito.kotlin.verify
+import org.mockito.kotlin.whenever
 
 class CarShouldTest {
 
     private val engine: Engine = mock()
 
     private val car = Car(5.0, engine)
+
+    init {
+        runBlockingTest {
+            whenever(engine.turnOn()).thenReturn(flow {
+                delay(2000)
+                emit(25.0)
+
+                delay(2000)
+                emit(50.0)
+
+                delay(2000)
+                emit(95.0)
+            })
+        }
+    }
 
     @get:Rule
     var coroutinesTestRule = MainCoroutineScopeRule()
