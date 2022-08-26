58465e671d5d31d3527c042d839a327e95629dc6
Oganbelema <oganbelema@gmail.com>
2022-08-26 15:50:13 +0100

    added delay to engine start and a default temperature for engine

--- app/src/main/java/com/oganbelema/unittestapplication/Engine.kt
+++ app/src/main/java/com/oganbelema/unittestapplication/Engine.kt
@@ -1,10 +1,13 @@
 package com.oganbelema.unittestapplication
 
-class Engine(var temperature: Double,
+class Engine(var temperature: Double = 15.0,
              var isTurnedOn: Boolean = false){
 
     fun turnOn() {
         isTurnedOn = true
+
+        Thread.sleep(6000)
+
         temperature = 95.0
     }
 
